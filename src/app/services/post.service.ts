18409fe5dc433b96fb522bb861822a0754ce37f8
luigi.capinpuyan <luigi.capinpuyan@pointwest.com.ph>
2023-11-29 14:11:34 +0800

    Polished current iteration of services. User service, friend service, and notification service to follow.

--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -4,6 +4,8 @@ import { Photo, PhotoDTO } from '../models/photo';
 import { Observable } from 'rxjs';
 import { PostDTO } from '../models/post';
 import { SessionService } from './session.service';
+import { MiniProfileDTO } from '../models/user';
+import { LikeDTO } from '../models/like';
 
 @Injectable({
   providedIn: 'root'
@@ -21,9 +23,30 @@ export class PostService {
   }
 
 
-  private baseUrl: string = 'https://localhost:7116/api/post'
+  private baseUrl: string = 'https://localhost:7208/api/post'
 
   addPost(postDTO: PostDTO): Observable<any> {
-    return this.http.post(`${this.baseUrl}/add-post/`, postDTO, {headers: this.header})
+    return this.http.post(`${this.baseUrl}/add-post/`, postDTO, {headers: this.header});
   }
+
+  updatePost(postDTO: PostDTO): Observable<any> {
+    return this.http.put(`${this.baseUrl}/update-post/`, postDTO, {headers: this.header});
+  }
+
+  deletePost(postId: string): Observable<Object> {
+    return this.http.delete(`${this.baseUrl}/delete-post/${postId}`, {headers: this.header});
+  }
+
+  getPost(postId: string): Observable<Object> {
+    return this.http.get(`${this.baseUrl}/get-post/${postId}`, {headers: this.header});
+  }
+
+  getPostLikes(postId: string): Observable<MiniProfileDTO[]>{
+    return this.http.get<MiniProfileDTO[]>(`${this.baseUrl}/get-post-likes/${postId}`, {headers: this.header});
+  }
+
+  likePost(likeDTO: LikeDTO): Observable<Object>{
+    return this.http.post(`${this.baseUrl}/like-post`, {headers: this.header});
+  }
+
 }
